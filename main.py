21ae7bef3e2b92f896597747696d96772d006a18
ulyses rico <ulyses.rico.2012@gmail.com>
2021-01-24 11:16:22 -0600

    ...

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print('here we go...')
+"""
+24-ene-2021
+Para mis pruebas usaré una lista de documentos pequeños
+Sobre todo para comprobar los cálculos manualmente, una vez seguro
+de los procedimientos, entonces funciona a gran escala
+"""
+
+doc1='Esta es una pequeña prueba para revisar.'
+doc2='Vamos a comprobar cosas sobre machine learning.'
+doc3='Todo debe tener sentido'
+doc4='Pongamos otro documento para que sea más información'
+lsDocs=[doc1,doc2,doc3]
+
+for doc in lsDocs:
+    print(doc)
