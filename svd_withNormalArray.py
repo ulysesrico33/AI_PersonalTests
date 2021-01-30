cfaaaf183f8f054011f2136b934ddd3131f31b75
ulyses rico <ulyses.rico.2012@gmail.com>
2021-01-29 21:59:17 -0600

    ...

--- svd_withNormalArray.py
+++ svd_withNormalArray.py
@@ -1,7 +1,8 @@
 from numpy import array
 from scipy.linalg import svd
+import numpy as np
 
-A=array([[1,2],[3,4],[5,6]])
+A=array([[1,2],[3,4]])
 print('Original Matriz A')
 print(A)
 U,s,V=svd(A)
@@ -11,3 +12,6 @@ print('Matrix s (Diagonal)')
 print(s)
 print('Matrix V (Right)')
 print(V)
+
+
+
